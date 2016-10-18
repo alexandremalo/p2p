e24b3d154d2ac5cfa7baef82a36583cd913eeeaa
alexandre <amalo@lanets.ca>
2016-10-18 15:09:21 +0200

    adding security.py

---
+++ security.py
@@ -0,0 +1,9 @@
+#Every host ID will be associate with a public/private key.
+#So everytime you want to talk to a host in the cluster, you need to know their public KEY
+#Ex:  If you join the cluster by connecting to 3 computers, you need to download the 3 public keys
+
+def encrypt_message_for_node(node_id, my_privatekey, node_publickey):
+	return None
+
+def decrypt_message_from_node(node_id, my_privatekey, node_publickey):
+	return None

36c337a0032e53b180bdde74aa8c255eddcdb0dc
alexandre <amalo@lanets.ca>
2016-10-18 15:29:34 +0200

    networking.py midified

--- networking.py
+++ networking.py
@@ -1,3 +1,7 @@
+import socket
+import glob
+import os
+
 def join_cluster(ip):
 	return None
 
@@ -37,3 +41,38 @@ def declare_dead_node(node_id):
 
 def print_routing_table():
 	print "not yet implemented"
+
+
+def listen_for_questions():
+        receiver_socket = socket.socket()
+        host = "172.20.20.20"
+        port = 5001
+        receiver_socket.bind((host, port))
+        receiver_socket.listen(5)
+        while True:
+                socket_obj, source = receiver_socket.accept()
+                print "Connection from ", source
+                message = socket_obj.recv(1024)
+		print message
+		socket_obj.sendall("Hello")
+
+def ask_question_to_node(question, id):
+	return None
+
+def send_message_to_node(message, id):
+
+def ask_question_to_directly_connected_node(question, host, port):
+        client_socket = socket.socket()
+        client_socket.connect((host, port))
+        client_socket.send(question)
+        return client_socket.recv(1024)
+
+def send_message_to_directly_connected_node(message, host, port):
+	client_socket = socket.socket()
+        client_socket.connect((host, port))
+        client_socket.send(message)
+
+print "starting P2P...."
+
+
+
